a5a9caa737160bf4f6b8a625242e37c4f1f4221e
anik <dascuet18.bd@gmail.com>
2025-08-21 14:34:08 +0600

    add getter and setter

--- src/main/java/org/example/gitrepo/UserEntity.java
+++ src/main/java/org/example/gitrepo/UserEntity.java
@@ -18,4 +18,22 @@ public class UserEntity {
 
         return username;
     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
 }
